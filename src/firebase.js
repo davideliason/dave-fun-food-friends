7874545cf3be02281e3185148f572d7d4ae99dcb
David Eliason <david@davethemaker.com>
2018-06-24 21:31:02 -0700

    hide api key and sensitive info using dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyCMmR1lWMoVaGhbo6Qjt_h_5JhTWqirp54",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "fun-food-friends-b0ba2.firebaseapp.com",
     databaseURL: "https://fun-food-friends-b0ba2.firebaseio.com",
     projectId: "fun-food-friends-b0ba2",
     storageBucket: "",
-    messagingSenderId: "932714540433"
+    messagingSenderId: process.env.MESSENGER_SENDING_ID
   };
 
 firebase.initializeApp(config);
